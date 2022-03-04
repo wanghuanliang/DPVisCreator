5d3cfa460fb8c61d05a92a1382eb7a73981bff81
wanghuanliang <415428346@qq.com>
2022-03-04 15:43:27 +0800

    feat: 代码结构调整

--- src/components/ModelView/ClockBlock/ClockBlock.jsx
+++ src/components/ModelView/ClockBlock/ClockBlock.jsx
@@ -3,10 +3,34 @@ import * as d3 from 'd3';
 
 const ClockBlock = () => {
 
+  const arc = d3.arc()
+    .innerRadius(0)
+    .outerRadius(10)
+    .startAngle(0)
+    .endAngle(Math.PI / 2);
+  const arc2 = d3.arc()
+    .innerRadius(10)
+    .outerRadius(15)
+    .startAngle(Math.PI / 4)
+    .endAngle(Math.PI);
+
   return (
-    <svg>
-      <g></g>
-    </svg>
+    <svg width={200} height={200}>
+      <g transform='translate(100,100)'>
+        <circle
+          r={10}
+          stroke="#cfd4dd"
+          fill='none'
+        ></circle>
+        <circle
+          r={15}
+          stroke="#cfd4dd"
+          fill='none'
+        ></circle>
+        <path d={arc()} stroke="#cfd4dd" fill='#f2d1cc' strokeWidth="1"></path>
+        <path d={arc2()} stroke="#cfd4dd" fill='#d4f2e5' strokeWidth="1"></path>
+      </g>
+    </svg> 
   )
 }
 
--- src/components/ModelView/ModalView.jsx
+++ src/components/ModelView/ModalView.jsx
@@ -3,10 +3,11 @@ import './ModalView.less';
 import { Switch, Slider, InputNumber, Row, Col, Space } from 'antd';
 import { CheckCircleOutlined, RightOutlined, CheckOutlined } from '@ant-design/icons';
 import * as d3 from 'd3'
+import ClockBlock from './ClockBlock/ClockBlock';
+import BayesianNetwork from './BayesianNetwork/BayyesianNetWork';
 
 const ModalView = (props) => {
   const [privacyBudgetValue, setPrivacyBudget] = useState(0.8)
-  const arcRef = useRef(null);
 
   const renderModalControlPanel = () => {
     return (
@@ -43,64 +44,39 @@ const ModalView = (props) => {
     )
   }
 
-    const arc = d3.arc()
-      .innerRadius(0)
-      .outerRadius(10)
-      .startAngle(0)
-      .endAngle(Math.PI / 2);
-    const arc2 = d3.arc()
-    .innerRadius(10)
-    .outerRadius(15)
-    .startAngle(Math.PI / 4)
-    .endAngle(Math.PI);
+  // const renderFlowChart = () => {
+  //   return (
+  //     <div className="flow-path">
+  //       <span>
+  //         {/* <CheckOutlined style={{color: '#000'}}/> */}
+  //       <CheckCircleOutlined style={{width:30, height: 30}}/>
+  //       Constraints
+  //       </span>
+  //       <span><RightOutlined /></span>
+  //       <span>
+  //         <CheckCircleOutlined style={{width:30, height: 30}}/>
+  //         Structure
+  //       </span>
+  //       <span><RightOutlined /></span>
+  //       <span>
+  //         <CheckCircleOutlined style={{width:30, height: 30}}/>
+  //         Parameter
+  //       </span>
+  //       <span><RightOutlined /></span>
+  //       <span>
+  //         <CheckCircleOutlined style={{width:30, height: 30}}/>
+  //       Sampling
+  //       </span>
+  //     </div>
+  //   )
+  // }
 
   return (
-    // <div>
-    //   {renderModalControlPanel()}
-    //   <div className="flow-path">
-    //     <span>
-    //       {/* <CheckOutlined style={{color: '#000'}}/> */}
-    //       <CheckCircleOutlined style={{width:30, height: 30}}/>
-    //       Constraints
-    //     </span>
-    //     <span><RightOutlined /></span>
-    //     <span>
-    //       <CheckCircleOutlined style={{width:30, height: 30}}/>
-    //       Structure
-    //     </span>
-    //     <span><RightOutlined /></span>
-    //     <span>
-    //       <CheckCircleOutlined style={{width:30, height: 30}}/>
-    //       Parameter
-    //     </span>
-    //     <span><RightOutlined /></span>
-    //     <span>
-    //       <CheckCircleOutlined style={{width:30, height: 30}}/>
-    //       Sampling
-    //     </span>
-    //   </div>
-    //   <div ref={chordDiagramRef}></div>
-    // </div>
     <div>
       {renderModalControlPanel()}
-      <svg width={600} height={400}>
-        <g transform='translate(100,100)'>
-          <circle
-            r={10}
-            stroke="#cfd4dd"
-            fill='none'
-          ></circle>
-          <circle
-            r={15}
-            stroke="#cfd4dd"
-            fill='none'
-          ></circle>
-          <path d={arc()} stroke="#cfd4dd" fill='#f2d1cc' strokeWidth="1"></path>
-          <path d={arc2()} stroke="#cfd4dd" fill='#d4f2e5' strokeWidth="1"></path>
-          {/* <path d="M 100 100 m 75 0 a 75 75 0 0 1 -75 75"
-          fill="red" stroke="green" stroke-width="10"/> */}
-        </g>
-      </svg>
+      {/* {renderFlowChart} */}
+      <ClockBlock></ClockBlock>
+      <BayesianNetwork></BayesianNetwork>
     </div>
   );
 };
